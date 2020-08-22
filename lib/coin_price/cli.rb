3086b835c838bf4fd760c6dd8263722f8601b65a
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-22 21:09:48 +0000

    stubbing out user interface

--- lib/coin_price/cli.rb
+++ lib/coin_price/cli.rb
@@ -1,8 +1,29 @@
 class CoinPrice::Cli
-  def greeting
-    puts "hi, and welcome to the coin price cli"
+  def call
+    puts "\nWelcome to the coin price cli!\n"
+    get_coins
+    list_coins
+    get_user_coin
+    #get_prices_for(coin)
+    #list prices
   end
   
-   
- 
-end 
+   def get_coins
+     #to be pulled from api instead
+     @coins = ["Bitcoin", "Ethereum", "Ripple"]
+    end
+    
+    def list_coins
+      #list coins
+      puts "\nChoose a Coin to see prices.\n"
+      @coins.each.with_index { |coin, index|
+      puts  "#{index + 1}. #{coin}"
+      }
+    end
+    
+    def get_user_coin
+      chosen_coin = gets.strip
+      
+    end
+    
+end  
