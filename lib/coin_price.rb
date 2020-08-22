18adb841fe411e31fe3f1f588d3b4e0c268f2740
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-22 20:29:46 +0000

    gem spec dependency

--- lib/coin_price.rb
+++ lib/coin_price.rb
@@ -2,6 +2,8 @@ require_relative  "../lib/coin_price/version"
 require_relative  "../lib/coin_price/cli"
 require_relative "../lib/coin_price/api"
 
+require 'pry'
+
 module CoinPrice
   class Error < StandardError; end
   # Your code goes here...
