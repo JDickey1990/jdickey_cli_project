470de6a1f2f178d7e374417aa70368991eaf0bd4
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-24 15:23:03 +0000

    removing price class

--- lib/coin_price.rb
+++ lib/coin_price.rb
@@ -2,7 +2,7 @@ require_relative  "../lib/coin_price/version"
 require_relative  "../lib/coin_price/cli"
 require_relative "../lib/coin_price/api"
 require_relative "../lib/coin_price/coin"
-require_relative "../lib/coin_price/price"
+
 
 require 'open-uri'
 require 'json'
